1f055f996a77f8a5cc641684a165f5f5548d7001
pashapushak1996 <pashapushak1996@gmail.com>
2023-04-23 02:53:03 +0300

    feat: added login into auth api

--- src/shared/api/auth.js
+++ src/shared/api/auth.js
@@ -9,11 +9,9 @@ export class ApiAuth {
       return null;
     }
 
-    console.log(JSON.stringify({ email, name, password, passwordConfirm }));
-
     try {
       const json = await Api.fetchRetry(`${BASE_API}/users/signup`, {
-        signal: ApiAbortController.genController('register').signal,
+        signal: ApiAbortController.genController('signUp').signal,
         headers: {
           'Content-Type': 'application/json',
         },
@@ -21,16 +19,39 @@ export class ApiAuth {
         body: JSON.stringify({ email, name, password, passwordConfirm }),
       });
 
-      ApiAbortController.clearController('register');
+      ApiAbortController.clearController('signUp');
 
       if (json.status !== 'success') {
         Api.handleErrorMessage(json);
       }
       const { data } = json;
 
-      localStorage.setItem('token', data.token);
+      return { user: data.user, token: data.token };
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
 
-      return { user: data.user };
+  static async login({ email, password }) {
+    try {
+      const json = await Api.fetchRetry(`${BASE_API}/users/login`, {
+        signal: ApiAbortController.genController('login').signal,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+      });
+
+      ApiAbortController.clearController('login');
+
+      if (json.status !== 'success') {
+        Api.handleErrorMessage(json);
+      }
+      const { data } = json;
+
+      return { token: data.token };
     } catch (err) {
       console.error(err);
       return null;
